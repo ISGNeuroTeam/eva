614dd57f685eb2dce38caa1a6add291eda675ab1
Sannikov_Aleksey <sannikov@holyweb.ru>
2022-08-01 16:19:38 +0200

    - Added font size settings on text-block in constructor schemes - Deleted old version yFiles
    https://app.clickup.com/t/2579894/DEV-2250

--- eva/src/yfiles/es-modules/lang.js
+++ eva/src/yfiles/es-modules/lang.js
@@ -1,10 +1,10 @@
 /****************************************************************************
  ** @license
- ** This file is part of yFiles for HTML 2.3.0.3.
+ ** This file is part of yFiles for HTML 2.4.0.5.
  **
  ** yWorks proprietary/confidential. Use is subject to license terms.
  **
- ** Copyright (c) 2020 by yWorks GmbH, Vor dem Kreuzberg 28,
+ ** Copyright (c) 2022 by yWorks GmbH, Vor dem Kreuzberg 28,
  ** 72070 Tuebingen, Germany. All rights reserved.
  **
  ***************************************************************************/
