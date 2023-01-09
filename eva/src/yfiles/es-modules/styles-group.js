67dbbfb6b5bfa6f1002cc6c19bf42abc102a60e7
Sannikov_Aleksey <sannikov@holyweb.ru>
2023-01-09 09:05:51 +0200

    Update yFiles, Save/load/build constructorSchemes
    - Update yFiles to v25.0.1
    - Update yFiles license to 2.4
    - Added saving of all elements in the "designer schema" as an array of objects with parameters for creating an element

---
+++ eva/src/yfiles/es-modules/styles-group.js
@@ -0,0 +1,11 @@
+/****************************************************************************
+ ** @license
+ ** This file is part of yFiles for HTML 2.5.0.1.
+ **
+ ** yWorks proprietary/confidential. Use is subject to license terms.
+ **
+ ** Copyright (c) 2022 by yWorks GmbH, Vor dem Kreuzberg 28,
+ ** 72070 Tuebingen, Germany. All rights reserved.
+ **
+ ***************************************************************************/
+import y from "./impl/styles-group.js";export var GroupNodeLabelModel=y.graph.GroupNodeLabelModel;export var GroupNodeStyle=y.styles.GroupNodeStyle;export var GroupNodeStyleRenderer=y.styles.GroupNodeStyleRenderer;export default y;
